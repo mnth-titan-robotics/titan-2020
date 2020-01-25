b7f033dd71b24a0417f3fba08f84cc8980cd6c80
bohdi00707 <43555313+bohdi00707@users.noreply.github.com>
2020-01-25 14:28:30 -0600

    Operator interface (#10)
    * Wrote OperatorInterface code
    
    * Updating Robot Constants
    
    * Commented Reasoning

--- src/main/java/frc/robot/RobotConstants.java
+++ src/main/java/frc/robot/RobotConstants.java
@@ -1,7 +1,7 @@
 package frc.robot;
 
 public class RobotConstants {
-    //public static final int EXAMPLE_CONSTANT = 1;
+    //Drive System
     public static final int MOTOR_CHANNEL_FL = 1;
     public static final int MOTOR_CHANNEL_FR = 2;
     public static final int MOTOR_CHANNEL_RL = 3;
@@ -9,4 +9,11 @@ public class RobotConstants {
 
     public static final boolean MOTOR_DRIVE_INVERT_L = false;
     public static final boolean MOTOR_DRIVE_INVERT_R = true;
+
+    //Operator Interface
+    public static final int JOYSTICK_PORT_PILOT = 0;
+    public static final int JOYSTICK_PORT_COPILOT = 1;
+
+    public static final int AXIS_DRIVE = 1;
+    public static final int AXIS_TURN = 4;
 }
