6d436c253c5f52045cd56b379b3a6d78369c9e66
bohdi00707 <nathanhowea@gmail.com>
2020-05-20 16:02:46 -0500

    minor housekeeping

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -18,7 +18,6 @@ import edu.wpi.first.cameraserver.*;
 import java.util.concurrent.TimeUnit;
 
 
-
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the TimedRobot
